d2a67aba6548d40e50f0bdbfba76b6e151bff824
Sean <miniod2018@gmail.com>
2021-09-06 02:25:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, redirect, flash, url_for, render_template
+from flask_cors import CORS
+import dbUtils
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+################################
+#   ROUTES
+################################
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template('index.jinja2')
+
+    if request.method == 'POST':
+        url = request.form['urlToAdd']
+        if not url:
+            urlCreated = request.form['urlCreated']
+            if urlCreated == "urlCreated":
+                return render_template('index.jinja2', url_created=False)
+
+            flash('The url is required')
+            return redirect(url_for('index'))
+        
+        result = dbUtils.create_url(url, request.base_url)    
+        if result == "No url":
+            flash('No url found')
+        else:
+            return render_template('index.jinja2', url_created=True, created_url=result)
+
+@app.route("/stats", methods=["GET"])
+def url_stats():
+    if request.method == 'GET':
+        return render_template('url_stats.jinja2')
+    if request.method == 'POST':
+        url_to_fetch = request.form['urlToFetch']
+        result = dbUtils.get_url_stats(url_to_fetch)
+        return render_template('url_stats.jinja2', stats=result)
+
+@app.route("/u/<string:url>", methods=["GET"])
+def get_url(url):
+    if url is not None:
+        result = dbUtils.get_url(url)
+        return redirect(result)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ dbUtils.py
@@ -0,0 +1,67 @@
+from pymongo import MongoClient, ReturnDocument
+from datetime import datetime as dt2
+from pytz import timezone
+import random
+import string
+import os
+
+tz = timezone('EST')
+
+def generate_id(N):
+    return_str = ''.join(random.choices(string.ascii_letters + string.digits, k=N))
+    check_hash(return_str)
+    return return_str
+
+def check_hash(hash):
+    urls = get_conn()
+    return True if urls.find({"url_id": hash}).count() > 0 else False
+
+def get_conn():
+    client = MongoClient(os.environ.get('MONGO_HOSTNAME'))
+    db = client.url_shortener
+    coll = db.urls
+    return coll
+
+def get_url(hash_id):
+    if hash_id is not None:
+        urls = get_conn()
+        starting_rec = urls.find_one({"url_id": hash_id})
+        times_visited = starting_rec['times_visited']
+
+        url_rec = urls.find_one_and_update(
+            {"url_id": hash_id}, 
+            { '$set': { "times_visited" : times_visited + 1, "last_visited": dt2.now(tz)} }, 
+            return_document = ReturnDocument.BEFORE
+        )
+        return url_rec['url']
+    else:
+        return "No URL recieved"
+
+
+def create_url(url, base_url):
+    if url is not None:
+        hash_id = generate_id(5)
+
+        urls = get_conn()
+        new_obj = {
+            "url_id": hash_id,
+            "url": url,
+            "date_created": dt2.now(tz),
+            "times_visited": 0,
+            "last_visited": dt2.now(tz)
+        }
+        try:
+            urls.insert_one(new_obj)
+            returnStr = base_url + "u/" + new_obj['url_id']
+        except Exception as e:
+            returnStr = f"{e},, {str(e)}"
+        return returnStr
+    else:
+        return "URL not recieved"
+
+
+def get_url_stats(hash_id):
+    if hash_id is not None:
+        urls = get_conn()
+        url_data = urls.find_one({"url_id": hash_id}) # TODO: setup
+        return url_data
